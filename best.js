7e867b474cf5e49fda2ebfa57c3d0cc54fdbe985
Elizabeth Pollard <station4tee@gmail.com>
2021-06-12 22:15:06 -0500

    IDK

--- best.js
+++ best.js
@@ -1,3 +1,30 @@
-var Elizabeth = 'You got this!';
-console.log(Elizabeth);
-console.log('We are ready!!');
+var arr = [3, 9, 23, 64, 2, 8, 28, 93];
+console.log(arr[arr.length -1] - arr[0]);
+arr.push(101);
+console.log(arr[arr.length - 1] - arr[0]);
+
+var names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'];
+var length = names.map(function(elements){
+return elements.length;
+});
+console.log(length);
+console.log(arr[arr.length -1]);
+console.log(arr[0]);
+
+function Seven(word, number){
+return word, number;
+}
+Seven("Hello", "Hello");
+
+
+
+
+
+function Eight(firstName, lastName){
+    console.log(firstName + ' ' + lastName);
+}
+    Eight( 'Elizabeth', "Pollard");
+
+    function willBuyDrink(isHotOutside, moneyInPocket){
+    if()
+}
